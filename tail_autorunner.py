20d56a441de888542b73b623649d3a659949b7b1
Alexander Makrigiorgos <amakri@cim.mcgill.ca>
2018-04-13 21:05:45 -0400

    overlap shift modification, change to tail creation process

--- tail_autorunner.py
+++ tail_autorunner.py
@@ -18,7 +18,9 @@ class tiler(object):
     
     def __init__(self):
         root = Tk()
-        root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+        root.withdraw()
+        currdir = os.getcwd()
+        root.filename =  filedialog.askopenfilename(initialdir = currdir,title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
         with open(root.filename, 'r') as f:
             reader = csv.reader(f)
             data = list(reader)
@@ -33,28 +35,41 @@ class tiler(object):
                 self.results[i][0] = data[i][0]
                 self.results[i][3] = data[i][0] + "_sense"
                 self.results[i][8] = data[i][0] + "_antisense"
-                hd, Tm = self.heterodimer(data[i][2],data[i][1])
+                hd, Tm_hd = self.heterodimer(data[i][2],data[i][1])
+                sense, Tm_s, pos = self.extend_sense(hd, data[i][2],data[i][1])
+                antisense, Tm_as = self.extend_antisense(hd, data[i][2],data[i][1])
+                as_pos = len(antisense) - 7
+
+                if (pos > 13 and (len(antisense) - 7) > 13):
+                	print(hd, sense, antisense)
+                	hd, sense, antisense, pos, as_pos = self.fixer(hd, sense, antisense, pos, as_pos)
+                	print(hd, sense, antisense)
+                	Tm_hd = self.analyze(hd)
+                	Tm_s = self.analyze(sense)
+                	Tm_as = self.analyze(antisense)
+                	print("Fixing " + str(i) + "'th entry...")
+
+                sense = self.add_tail(data[i][2], sense, 1)
+                antisense = self.add_tail(data[i][2], antisense, 0)
+
+
                 self.results[i][1] = hd
-                self.results[i][2] = int(Tm)
+                self.results[i][2] = int(Tm_hd)
                 
-                sense, Tm, pos = self.extend_sense(hd, data[i][2],data[i][1])
                 self.results[i][4] = sense
-                self.results[i][5] = int(Tm)
+                self.results[i][5] = int(Tm_s)
                 self.results[i][6] = len(sense)
                 self.results[i][7] = pos  
-                #self.results[i][14] = sense[:-3]
                 
-                antisense, Tm = self.extend_antisense(hd, data[i][2],data[i][1])
                 copy = antisense
                 self.results[i][13] = copy
                 antisense = list(antisense)
                 antisense.reverse()
                 antisense = ''.join(antisense)
                 self.results[i][9] = self.convert(antisense)
-                self.results[i][10] = int(Tm)
+                self.results[i][10] = int(Tm_as)
                 self.results[i][11] = len(antisense)
-                self.results[i][12] = len(antisense) - 10 
-                #self.results[i][15] = self.convert(antisense[:-3])
+                self.results[i][12] = as_pos
                 
             except IndexError:
                 i = i-1
@@ -68,7 +83,11 @@ class tiler(object):
         
         #Results: [0 Target_name, 1 Probe Overlap Dimer, 2 Tm, 3 Target_name_sense, 4 Sense_tail, 5 Tm, 6 Length, 
         #			7 Mut_position, 8 Target_name_antisense, 9 Antisense_tail, 10 Tm, 11 Length, 12 Mutation Position
-        #           13 Reverse Complement, 14 Sense_no_tail, 15 Antisense_no_tail]
+        #           13 Reverse Complement]
+
+        # Processing results for mutation positions over 15 #
+        # Remove but save tail
+
         name = root.filename[:-4] + "_results_tail.csv"
         f = open(name, "w")
         writer = csv.writer(f)
@@ -82,6 +101,10 @@ class tiler(object):
         	self.results2[i][6] = int(self.results2[i][6]) - 3
         	self.results2[i][9] = self.results2[i][9][:-3]
         	self.results2[i][11] = int(self.results2[i][11]) - 3
+        	rc = list(self.results2[i][9])
+        	rc.reverse()
+        	rc = ''.join(rc)
+        	self.results2[i][13] = self.convert(rc)
 
 
         name = root.filename[:-4] + "_results_notail.csv"
@@ -91,7 +114,57 @@ class tiler(object):
         for i in range(len(self.results2)):
         	writer.writerow(self.results2[i])
 
+    def fixer(self, overlap, sense, antisense, sense_pos, antisense_pos):
+    	overlap = list(overlap)
+    	sense = list(sense)
+    	antisense = list(antisense)
+    	if sense_pos < antisense_pos:
+    		while sense_pos > 13:
+    			len_overlap = len(overlap)
+    			overlap.append(antisense[len_overlap+1])
+    			sense.append(overlap[-1])
+    			sense = sense[1:]
+    			sense_pos = sense_pos - 1
+    	else:
+    		while antisense_pos > 13:
+    			len_overlap = len(overlap)
+    			overlap.insert(0, sense[len(sense) - len_overlap - 1])
+    			antisense.insert(0, sense[len(sense) - len_overlap - 1])
+    			antisense = antisense[:-1]
+    			antisense_pos = antisense_pos - 1
+    	overlap = ''.join(overlap)
+    	sense = ''.join(sense)
+    	antisense = ''.join(antisense)
+    	return overlap, sense, antisense, sense_pos, antisense_pos
+
         
+    def add_tail(self, seq, probe, sense_flag):
+    	# Regexp match seq and probe
+    	# Go to indices after the match and get tail bases from there
+
+    	if sense_flag == 1:
+    		end = seq.find(probe) + len(probe)
+    		for i in range(3):
+    			probe = list(probe)
+    			if seq[end] != 'A':
+    				probe.append('A')
+    			else:
+    				probe.append('C')
+    			end = end + 1
+    			probe = ''.join(probe)
+    	else:
+    		end = seq.find(probe) - 1
+    		for i in range(3):
+    			probe = list(probe)
+    			if seq[end] != 'T':
+    				probe.insert(0, 'T')
+    			else:
+    				probe.insert(0, 'G')
+    			end = end - 1 
+    			probe = ''.join(probe)
+
+    	return probe
+
     ##Attaches bases to antisense probe until TM is satisfactory
     def extend_antisense(self, hd, seq, start):
         antisense = []
@@ -112,15 +185,11 @@ class tiler(object):
             Tm = self.analyze(antisense)
             #print Tm
         
-        end = end - 1
-        for i in range(3):
+        while len(antisense) < 15:
         	antisense = list(antisense)
-        	if seq[end] != 'T':
-        		antisense.insert(0, 'T')
-        	else:
-        		antisense.insert(0, 'G')
-        	end = end - 1
+        	antisense.append(seq[probe_lengthener])
         	antisense = ''.join(antisense)
+        	probe_lengthener = probe_lengthener + 1
 
         return (antisense, Tm)
     
@@ -144,16 +213,14 @@ class tiler(object):
             count = count + 1
             
             Tm = self.analyze(sense)
-            
-        end = end + 1
-        for i in range(3):
+        
+        while len(sense) < 15:
         	sense = list(sense)
-        	if seq[end] != 'A':
-        		sense.append('A')
-        	else:
-        		sense.append('C')
-        	end = end + 1
+        	sense.insert(0, seq[probe_lengthener])
         	sense = ''.join(sense)
+        	probe_lengthener = probe_lengthener - 1
+        	count = count + 1
+
         ## Tm should never be > MAX_PROBE_TM because it starts with hd's tm, which is significantly lower, no second while required
         return (sense, Tm, count)
     
